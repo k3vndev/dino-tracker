71acd016a13cef4ac0eb62dd8b73687b9b52f0e9
K3vnDev <kevinrpolo16@gmail.com>
2026-04-20 11:47:18 -0500

    feat: enhance animations for landing page components; add animation delays for smoother transitions

--- src/components/landing/cta-button.tsx
+++ src/components/landing/cta-button.tsx
@@ -1,7 +1,13 @@
 import { LinkButton } from '@components'
+import { cn } from '@utils'
 
-export const CTAButton = () => (
-  <LinkButton primary icon='arrow' className='mt-12' href='/projects'>
+export const CTAButton = ({ className = '' }) => (
+  <LinkButton
+    primary
+    icon='arrow'
+    className={cn('mt-12 animation-slide-in-from-bottom', className)}
+    href='/projects'
+  >
     Take control of your time
   </LinkButton>
 )
